38c471f1ee2f01a59a3e0c7452b505a0637e014c
Brahian Velázquez Téllez <brahian.velazquez@BVELAZQUEZES60W10.babel-si.local>
2020-10-08 15:35:19 -0500

    added listTodos entity

--- frontend/todo/src/app/welcome/welcome.component.ts
+++ frontend/todo/src/app/welcome/welcome.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AppComponent } from '../app.component';
 
+
+
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
