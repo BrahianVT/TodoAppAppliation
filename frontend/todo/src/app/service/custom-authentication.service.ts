979dc20b34a1194e022e0cd68d4e747cfcf0e786
Brahian Velázquez Téllez <brahian.velazquez@BVELAZQUEZES60W10.babel-si.local>
2020-10-12 16:23:27 -0500

    modified authentication service

--- frontend/todo/src/app/service/custom-authentication.service.ts
+++ frontend/todo/src/app/service/custom-authentication.service.ts
@@ -7,9 +7,17 @@ export class CustomAuthenticationService {
 
   constructor() { }
 
-  authenticate(username :String, password :String){
+  authenticate(username :string, password :string){
+    console.log('before' + this.isUserLoggegIn());
     if(username === 'Brahian' && password === 'xd'){
+      sessionStorage.setItem('authenticateUser', username);
+      console.log('After ' + this.isUserLoggegIn());
       return true;
     } else return false;
+  }
+
+  isUserLoggegIn(){
+    let user = sessionStorage.getItem('authenticateUser');
+    return user !== null;
   }
 }
