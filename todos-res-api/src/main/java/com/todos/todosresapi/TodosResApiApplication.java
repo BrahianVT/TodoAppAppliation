3febb621b232b18b2a1487c1947cae744e83a191
Brahian Velázquez Téllez <brahian.velazquez@BVELAZQUEZES60W10.babel-si.local>
2020-10-13 17:35:37 -0500

    Added Rest API project (structure)

---
+++ todos-res-api/src/main/java/com/todos/todosresapi/TodosResApiApplication.java
@@ -0,0 +1,13 @@
+package com.todos.todosresapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosResApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodosResApiApplication.class, args);
+	}
+
+}
---
+++ todos-res-api/src/test/java/com/todos/todosresapi/TodosResApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todos.todosresapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosResApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
